b730e7f7ff83ddc92068cf33601981d3179e5bf2
Mohamed Sharif <e0540641@u.nus.edu>
2026-03-29 16:04:14 +0800

    Update dev files to perform checks

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -1,1 +1,20 @@
-../main.tf
+terraform {
+  required_version = ">= 1.4.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "terraform-state-345895787413-ap-southeast-1-an"
+    key    = "dev/terraform.tfstate"
+    region = "ap-southeast-1"
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
