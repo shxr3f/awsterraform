2abe3613d605a9e42f2ad81249b37957ea609c02
Mohamed Sharif <e0540641@u.nus.edu>
2026-03-29 16:05:38 +0800

    Fixing path to module

--- terraform/dev/test.tf
+++ terraform/dev/test.tf
@@ -1,7 +1,7 @@
 data "aws_caller_identity" "current" {}
 
 module "test_bucket" {
-  source = "../modules/s3_bucket"
+  source = "../../modules/s3_bucket"
 
   name       = "test"
   account_id = data.aws_caller_identity.current.account_id
